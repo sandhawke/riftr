435b4ef244f2ac6519c21b0f2e88fb3953e190e4
Sandro Hawke <sandro@hawke.org>
2009-02-03 06:06:07 +0000

    skeleton is complete...   does it work?

--- webapp.py
+++ webapp.py
@@ -19,6 +19,7 @@ import ply.lex as lex
 
 import ps_parse
 import ps_lex
+import xml_in
 import rif
 
 #
@@ -45,7 +46,7 @@ def startPage(title):
         page.head << h.stylelink("http://validator.w3.org/base.css")
 
 
-def main_page(input=None):
+def main_page(input=None, action="PS to PS"):
     global page
 
     startPage("RIF (Highly Experimental) Demonstration Page")	
@@ -55,51 +56,46 @@ def main_page(input=None):
     form = h.form(method="GET", class_="f")	
     form << h.h3("Input Text...") 
     if input is None:
-        input = """Document(
-  Prefix(cpt <http://example.com/concepts#>)
-  Prefix(ppl <http://example.com/people#>)
-  Prefix(bks <http://example.com/books#>)
-
-  Group
-  (
-    Forall ?Buyer ?Item ?Seller (
-        cpt:buy(?Buyer ?Item ?Seller) :- cpt:sell(?Seller ?Item ?Buyer)
-    )
- 
-    cpt:sell(ppl:John bks:LeRif ppl:Mary)
-  )
-)
-    """
+        input = default_input()
     form << h.textarea(input,
                        cols="90", rows="20", name="input")
     form << h.br()
-    form <<  h.input(type="submit",  name="p_p", value="PS->PS")
-    form <<  h.input(type="submit",  name="p_x", value="PS->XML")
-    form <<  h.input(type="submit",  name="x_x", value="XML->XML")
-    form <<  h.input(type="submit",  name="x_p", value="XML->PS")
+    form <<  h.input(type="submit",  name="action", value="PS to PS")
+    form <<  h.input(type="submit",  name="action", value="PS to XML")
+    form <<  h.input(type="submit",  name="action", value="XML to XML")
+    form <<  h.input(type="submit",  name="action", value="XML to PS")
     page << form
 
     page << h.h3('Translates to...')
 
-    output = translate(input)
+    output = translate(input, action)
 
     page << h.pre(output, style="padding:0.5em; border: 2px solid black;")
+
+    page << h.p("This page/software was developed by sandro@w3.org.   It's too buggy right now to use.   Please don't even bother to report bugs.")
 
     print page
     # cgi.print_environ()    
 
-def translate(input):
+def translate(input, action):
     
     s = input
-    try:
-        result = ps_parse.parse(s)
-    except SyntaxError, e:
-        result = "Parser Error"
-    except lex.LexError, e:
-        result = "Lexer Error"
 
-    return result.as_ps()
+    if action.startswith("PS to "):
+        doc = ps_parse.parse(s)
+    elif action.startswith("XML to "):
+        p = xml_in.Parser(rif.bld_schema)
+        p.root = ET.fromstring(s)
+        doc = p.instance_from_XML(p.root)
+    else:
+        raise RuntimeError('not a valid source format')
 
+    if action.endswith(" to PS"):
+        return rif.as_ps(doc)
+    elif action.endswith("XML to "):
+        return bld_xml_out.do(doc)
+    else:
+        raise RuntimeError('not a valid output format')
 
 
 def ensure_safety(uri):
@@ -116,6 +112,7 @@ def cgiMain():
 
     form = cgi.FieldStorage()
     input=form.getfirst("input")
+    action=form.getfirst("action")
     main_page(input)
     #if input is None or input == "":
     #    prompt()
@@ -124,5 +121,20 @@ def cgiMain():
     #    run(input)
 
 
+def default_input() :
+        return """Document(
+  Prefix(cpt <http://example.com/concepts#>)
+  Prefix(ppl <http://example.com/people#>)
+  Prefix(bks <http://example.com/books#>)
 
+  Group
+  (
+    Forall ?Buyer ?Item ?Seller (
+        cpt:buy(?Buyer ?Item ?Seller) :- cpt:sell(?Seller ?Item ?Buyer)
+    )
+ 
+    cpt:sell(ppl:John bks:LeRif ppl:Mary)
+  )
+)
+    """
 # http://www.w3.org/RDF/Validator/
