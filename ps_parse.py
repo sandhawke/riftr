bb2577995426ea953b1e9df8f98fabbae5b9d1cb
Sandro Hawke <sandro@hawke.org>
2009-02-04 17:38:05 +0000

    ready for using AST, with one switch

--- ps_parse.py
+++ ps_parse.py
@@ -17,8 +17,20 @@ import error
 import ps_lex
 import plugin
 
+def node(type, **kwargs):
+   cls = getattr(rif, type)
+   return cls(**kwargs)
+
+#def node(type, **kwargs):
+#   return AST.Node(type, **kwargs)
+
+def iri(part1, part2=""):
+   """node('IRI', RIFNS+"local"
+   """
+   return node('IRI', text=(part1+part2))
+
 RIFNS = "http://www.w3.org/2007/rif#"
-RIF_IRI= rif.IRI(RIFNS+"iri")
+RIF_IRI= iri(RIFNS, "iri")
 RDFNS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
 XSDNS = "http://www.w3.org/2001/XMLSchema#"
 
@@ -26,7 +38,7 @@ tokens = ps_lex.tokens
 
 def p_Document(t):
    '''Document  : IRIMETA_opt KW_Document LPAREN Base_opt Prefix_star Import_star Group_opt RPAREN '''
-   t[0] = rif.Document(annotation=t[1], base=t[4], prefix=t[5], imports=t[6], payload=t[7])
+   t[0] = node('Document', annotation=t[1], base=t[4], prefix=t[5], imports=t[6], payload=t[7])
 
 def p_Base(t):
    '''Base      : KW_Base LPAREN STRING RPAREN '''   
@@ -45,7 +57,7 @@ def p_Import(t):
    '''Import    : IRIMETA_opt KW_Import LPAREN LOCATION PROFILE_opt RPAREN '''
    location = t[4]
    profile = t[5]
-   t[0] = rif.Import(IRI(location), profile)
+   t[0] = node('Import', location=iri(location), profile=profile)
 
 def p_LOCATION(t):
    '''LOCATION    : ANGLEBRACKIRI
@@ -55,11 +67,11 @@ def p_LOCATION(t):
 
 def p_Group(t):
    '''Group     : IRIMETA_opt KW_Group LPAREN RULE_or_Group_star RPAREN '''
-   t[0] = rif.Group(annotation=t[1], sentence=t[4])
+   t[0] = node('Group', annotation=t[1], sentence=t[4])
 
 def p_RULE_1(t):
    '''RULE      : IRIMETA_opt KW_Forall Var_plus LPAREN CLAUSE RPAREN'''
-   t[0] = rif.Forall(annotation=t[1], declare=t[3], formula=t[5])
+   t[0] = node('Forall', annotation=t[1], declare=t[3], formula=t[5])
 
 def p_RULE_2(t):
    '''RULE      : CLAUSE '''
@@ -77,13 +89,13 @@ def p_CLAUSE_2(t):
 
 def p_Implies_1(t):
    '''Implies   :  ATOMIC COLONDASH FORMULA'''
-   t[0] = rif.Implies(then=t[1], if_=t[3])
+   t[0] = node('Implies', then=t[1], if_=t[3])
 
 def p_Implies_2(t):
    # is the metadata on the AND or the Rule?
    # ...remove IRIMETA_opt
    '''Implies   : KW_And LPAREN ATOMIC_star RPAREN COLONDASH FORMULA '''
-   t[0] = rif.Implies(then=rif.And(formula=t[3]), if_=t[6])
+   t[0] = node('Implies', then=node('And', formula=t[3]), if_=t[6])
 
 
 def p_PROFILE(t):
@@ -92,24 +104,24 @@ def p_PROFILE(t):
 
 def p_FORMULA_1(t):
    '''FORMULA        :  IRIMETA_opt KW_And LPAREN FORMULA_star RPAREN'''
-   t[0] = rif.And(annotation=t[1], formula=t[4])
+   t[0] = node('And', annotation=t[1], formula=t[4])
 def p_FORMULA_2(t):
    '''FORMULA        :  IRIMETA_opt KW_Or LPAREN FORMULA_star RPAREN'''
-   t[0] = rif.Or(annotation=t[1], formula=t[4])
+   t[0] = node('Or', annotation=t[1], formula=t[4])
 def p_FORMULA_3(t):
    '''FORMULA        :  IRIMETA_opt KW_Exists Var_plus LPAREN FORMULA RPAREN'''
-   t[0] = rif.Exists(annotation=t[1], declare=t[3], formula=t[5])
+   t[0] = node('Exists', annotation=t[1], declare=t[3], formula=t[5])
 def p_FORMULA_4(t):
    '''FORMULA        :  ATOMIC'''
    t[0] = t[1]
 def p_FORMULA_5(t):
    '''FORMULA        :  IRIMETA_opt KW_External LPAREN Atom RPAREN'''
-   t[0] = rif.ExternalAtom(annotation=t[1], content=t[4])
+   t[0] = node('ExternalAtom', annotation=t[1], content=t[4])
 
 #   -- being removed from Spec
 #def p_FORMULA_5(t):
 #   '''FORMULA        :  IRIMETA_opt KW_External LPAREN Frame RPAREN'''
-#   t[0] = rif.
+#   t[0] = node('
 
 
 def p_ATOMIC(t):
@@ -125,12 +137,12 @@ def p_ATOMIC(t):
 def p_Atom_1(t):
    # was UNITERM
    '''Atom           : Const LPAREN TERM_star RPAREN'''
-   t[0] = rif.Atom(op=t[1], args=t[3])
+   t[0] = node('Atom', op=t[1], args=t[3])
 
 def p_Atom_2(t):
    # was UNITERM
    '''Atom           : Const LPAREN Name_arrow_TERM_plus RPAREN '''
-   t[0] = rif.NamedArgsAtom(op=t[1], slot=t[3])
+   t[0] = node('NamedArgsAtom', op=t[1], slot=t[3])
 
 # put in-line
 #def p_UNITERM(t):
@@ -141,22 +153,22 @@ def p_Atom_2(t):
 
 def p_Equal(t):
    '''Equal          : TERM EQUALS TERM '''
-   t[0] = rif.Equal(left=t[1], right=t[3])
+   t[0] = node('Equal', left=t[1], right=t[3])
 
 
 def p_Member(t):
    '''Member         : TERM HASH TERM '''
-   t[0] = rif.Member(instance=t[1], class_=t[3])
+   t[0] = node('Member', instance=t[1], class_=t[3])
 
 
 def p_Subclass(t):
    '''Subclass       : TERM HASHHASH TERM '''
-   t[0] = rif.Subclass(sub=t[1], super=t[3])
+   t[0] = node('Subclass', sub=t[1], super=t[3])
 
 
 def p_Frame(t):
    '''Frame          : TERM LBRACKET TERM_arrow_TERM_star RBRACKET '''
-   t[0] = rif.Frame(object=t[1], slot=t[3])
+   t[0] = node('Frame', object=t[1], slot=t[3])
 
 def p_TERM_1(t):
    '''TERM           : IRIMETA_opt Const
@@ -167,16 +179,16 @@ def p_TERM_1(t):
    
 def p_TERM_2(t):
    '''TERM           : IRIMETA_opt KW_External LPAREN Expr RPAREN '''
-   t[0] = rif.ExternalExpr(annotation=t[1], content=t[4])
+   t[0] = node('ExternalExpr', annotation=t[1], content=t[4])
 
 # was UNITERM
 def p_Expr_1(t):
    '''Expr           : Const LPAREN TERM_star RPAREN '''
-   t[0] = rif.Expr(op=t[1], args=t[3])
+   t[0] = node('Expr', op=t[1], args=t[3])
 
 def p_Expr_2(t):
    '''Expr           : Const LPAREN Name_arrow_TERM_plus RPAREN '''
-   t[0] = rif.NamedArgsExpr(op=t[1], slot=t[3])
+   t[0] = node('NamedArgsExpr', op=t[1], slot=t[3])
 
 def p_Const_1(t):
    '''Const          : CONSTSHORT '''
@@ -184,32 +196,32 @@ def p_Const_1(t):
 
 def p_Const_2(t):
    '''Const          : STRING_HAT_HAT SYMSPACE '''
-   t[0] = rif.Const(lexrep=t[1], datatype=rif.IRI(t[2]))
+   t[0] = node('Const', lexrep=t[1], datatype=iri(t[2]))
 
 # MINE
 def p_CONSTSHORT_1(t):
    '''CONSTSHORT    : STRING'''
-   t[0] = rif.Const(datatype=rif.IRI(XSDNS+"string"), lexrep=t[1])
+   t[0] = node('Const', datatype=iri(XSDNS+"string"), lexrep=t[1])
 def p_CONSTSHORT_2(t):
    '''CONSTSHORT    : INTEGER'''
-   t[0] = rif.Const(datatype=rif.IRI(XSDNS+"integer"), lexrep=t[1])
+   t[0] = node('Const', datatype=iri(XSDNS+"integer"), lexrep=t[1])
 def p_CONSTSHORT_3(t):
    '''CONSTSHORT    : DECIMAL'''
-   t[0] = rif.Const(datatype=rif.IRI(XSDNS+"decimal"), lexrep=t[1])
+   t[0] = node('Const', datatype=iri(XSDNS+"decimal"), lexrep=t[1])
 def p_CONSTSHORT_4(t):
    '''CONSTSHORT    : ANGLEBRACKIRI
                     | BARE_IRI
                     | EXPANDED_CURIE'''
-   t[0] = rif.Const(datatype=RIF_IRI, lexrep=t[1])
+   t[0] = node('Const', datatype=RIF_IRI, lexrep=t[1])
 def p_CONSTSHORT_5(t):
    '''CONSTSHORT    : LOCALNAME'''
-   t[0] = rif.Const(datatype=rif.IRI(RIFNS+"local"), lexrep=t[1])
+   t[0] = node('Const', datatype=iri(RIFNS+"local"), lexrep=t[1])
 
 
 def p_Var(t):
    '''Var            : QUESTION STRING 
                      | QUESTION LOCALNAME '''
-   t[0] = rif.Var(name=t[2])
+   t[0] = node('Var', name=t[2])
 
 
 
@@ -235,10 +247,10 @@ def p_EXPANDED_CURIE(t):
 def p_IRICONST_1(t):
    '''IRICONST       : ANGLEBRACKIRI
                      | EXPANDED_CURIE'''
-   t[0] = rif.Const(datatype=RIF_IRI, lexrep=t[1])
+   t[0] = node('Const', datatype=RIF_IRI, lexrep=t[1])
 def p_IRICONST_2(t):
    '''IRICONST       : STRING_HAT_HAT SYMSPACE'''
-   t[0] = rif.Const(datatype=rif.IRI(t[2]), lexrep=t[1])
+   t[0] = node('Const', datatype=iri(t[2]), lexrep=t[1])
    require_iri(t)
 
 def require_iri(t):
@@ -253,21 +265,21 @@ def p_IRIMETA_opt_1(t):
    pass
 def p_IRIMETA_opt_2(t):
    '''IRIMETA_opt     : LMETA IRICONST RMETA '''
-   t[0] = rif.Annotation(iri=t[2], sentence=None)
+   t[0] = node('Annotation', iri=t[2], sentence=None)
 def p_IRIMETA_opt_3(t):
    '''IRIMETA_opt     : LMETA IRICONST KW_And LPAREN Frame_star RPAREN RMETA '''
-   t[0] = rif.Annotation(iri=t[2], sentence=rif.And(formula=t[5]))
+   t[0] = node('Annotation', iri=t[2], sentence=node('And', formula=t[5]))
 # These next two are tricky; we need both of them to work around the
 # ambiguity here.
 def p_IRIMETA_opt_4(t):
    '''IRIMETA_opt     : LMETA IRICONST LBRACKET TERM_arrow_TERM_star RBRACKET RMETA '''
-   t[0] = rif.Annotation(sentence=rif.Frame(object=t[2], slot=t[4]))
+   t[0] = node('Annotation', sentence=node('Frame', object=t[2], slot=t[4]))
 def p_IRIMETA_opt_5(t):
    '''IRIMETA_opt     : LMETA TERM LBRACKET TERM_arrow_TERM_star RBRACKET RMETA '''
-   t[0] = rif.Annotation(sentence=rif.Frame(object=t[2], slot=t[4]))
+   t[0] = node('Annotation', sentence=node('Frame', object=t[2], slot=t[4]))
 def p_IRIMETA_opt_6(t):
    '''IRIMETA_opt     : LMETA IRICONST TERM LBRACKET TERM_arrow_TERM_star RBRACKET  RMETA'''
-   t[0] = rif.Annotation(iri=t[2], sentence=rif.Frame(object=t[3], slot=t[5]))
+   t[0] = node('Annotation', iri=t[2], sentence=node('Frame', object=t[3], slot=t[5]))
 
 # This rule produces all our shift/reduce conflicts.   Sigh.
 # Because there are several places you could put meta, there are
@@ -280,7 +292,7 @@ def p_IRIMETA_opt_7(t):
 
 def p_TERM_arrow_TERM(t):
    '''TERM_arrow_TERM : TERM ARROW TERM '''
-   t[0] = rif.Slot(key=t[1], value=t[3])
+   t[0] = node('Slot', key=t[1], value=t[3])
 
 
 def p_RULE_or_Group(t):
@@ -291,7 +303,7 @@ def p_RULE_or_Group(t):
 
 def p_Name_arrow_TERM(t):
    '''Name_arrow_TERM : NAME_ARROW TERM '''
-   t[0] = rif.Slot(key=t[1], value=t[3])
+   t[0] = node('Slot', key=t[1], value=t[3])
 
 
 #def p_Frame_or_AndFrame_1(t):
@@ -300,7 +312,7 @@ def p_Name_arrow_TERM(t):
 #
 #def p_Frame_or_AndFrame_2(t):
 #   '''Frame_or_AndFrame : KW_And LPAREN Frame_star RPAREN '''
-#   t[0] = rif.And(formula=t[3])
+#   t[0] = node('And', formula=t[3])
 
 
 # Kleene star (*) expansion productions
