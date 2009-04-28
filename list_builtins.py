547f2c9dad16d6f5f4f9996e1e1dd85585b4c0b3
Sandro Hawke <sandro@hawke.org>
2009-04-28 04:29:23 +0000

    generated the wiki page

--- list_builtins.py
+++ list_builtins.py
@@ -1,4 +1,3 @@
-
 """
 ==== <tt>func:numeric-add</tt> (adapted from <tt>[http://www.w3.org/TR/xpath-functions/#func-numeric-add  op:numeric-add]</tt>) ====
 <ul>
@@ -40,6 +39,9 @@ class Builtin:
     def name(cls):
         return cls.__name__.replace('__', '').replace('_', '-')
 
+    from_xpath = True
+    optional = []
+
 class Builtin_Predicate (Builtin) :
     
     is_builtin = True
@@ -52,9 +54,10 @@ class Builtin_Function (Builtin) :
 
 
 class is_list ( Builtin_Predicate ):
-    """A guard predicate, true only for lists.
-    """
+    """A guard predicate, true if ?object is a list, and false otherwise."""
     args = ['object']
+
+    from_xpath = False
 
     examples = [
         Example([[0,1,2,3]], True),
@@ -70,25 +73,22 @@ class is_list ( Builtin_Predicate ):
 class deep_equal ( Builtin_Predicate ):
 
     """True only if list_1 and list_2 are both lists, are both the
-    same length, and each item (in order) is compares as equal using
-    using the proper datatype equality predicate.
+    same length, and each item (in order) compares as equal using
+    the proper datatype equality predicate (for the list_1 item).
     """
 
     issue = """What if the datatype of list_1[i] says they are equal and the datatype of list_2[i] says they are not?"""
 
-    args = ['list_1', 'list_2']
+    args = ['list_1', 'list_2', 'collation']
+    optional = ['collation']
 
     examples = [
         Example([[0,1,2,3,4], []], False),
         Example([[0,1,2,3,4], [1]], False),
-        Example([[0,1,2,3,4], [0,1,2,3,4,4]], False),
         Example([[0,1,2,3,4], [0,1,2,3,4]], True),
-        Example([[0,1,2,3,4], [0,1,2,3,4,6]], False),
-        Example([[0,1,2,3,4], [2]], False),
-        Example([[0,1,2,3,[0,1,2], 4], [0,1,2,3,[0,1,1], 4]], False),
+        Example([[0,1,2,3,4], [0,1,2,3,4,5]], False),
         Example([[0,1,2,3,[0,1,2], 4], [0,1,2,3,[0,1,2], 4]], True),
         Example([[0,1,2,3,[0,1,2], 4], [0,1,2,3,[0,1,3], 4]], False),
-
         ]
 
     @classmethod
@@ -105,10 +105,10 @@ class deep_equal ( Builtin_Predicate ):
 
 class concatenate ( Builtin_Function ):
     """
-    Returns a new list consisting of all the items in the first list followed by all the items in the second list.
+    Returns a new list consisting of all the items in list-1 followed by all the items in list-2.
     """
 
-    args = ['list_1', 'list_2']
+    args = ['list-1', 'list-2']
     result = 'new-list'
     issue="This is a poor name.   'concat' is used for strings; perhaps 'list-concat' would be better, but XPath uses 'concatenate'"
 
@@ -117,20 +117,20 @@ class concatenate ( Builtin_Function ):
         return first_list + second_list
 
 class insert_before ( Builtin_Function ):
-    """Return a new list which is the old-list with the new-item being inserted at the given position; the item in the old-list at that position (if any) and any following items are shifted down one position."""
+    """Return a new list which is ?old-list with ?new-item being inserted at the given ?position.  The item in ?old-list which was at that position (if any) and any following items are shifted down one position."""
 
     args =  ['old-list', 'position', 'new-item']
     result = 'new-list'
 
     examples = [
-        Example([[0,1,2,3,4], 0, 99], [99, 1,2,3,4]),
-        Example([[0,1,2,3,4], 1, 99], [0,1, 99, 2,3,4]),
-        Example([[0,1,2,3,4], 5, 99], [0,1, 2,3,4,5, 99]),
-        Example([[0,1,2,3,4], 6, 99], [0,1, 2,3,4,5, 99]),
-        Example([[0,1,2,3,4], 10, 99], [0,1, 2,3,4,5, 99]),
-        Example([[0,1,2,3,4], -1, 99], [0,1, 2,3,4, 99, 5]),
-        Example([[0,1,2,3,4], -5, 99], [99, 1, 2,3,4, 5]),
-        Example([[0,1,2,3,4], -10, 99], [99, 1, 2,3,4, 5]),
+        Example([[0,1,2,3,4], 0, 99], [99, 0,1,2,3,4]),
+        Example([[0,1,2,3,4], 1, 99], [0,99, 1, 2,3,4]),
+        Example([[0,1,2,3,4], 5, 99], [0,1, 2,3,4, 99]),
+        Example([[0,1,2,3,4], 6, 99], [0,1, 2,3,4, 99]),
+        Example([[0,1,2,3,4], 10, 99], [0,1, 2,3,4, 99]),
+        Example([[0,1,2,3,4], -1, 99], [0,1, 2,3, 99, 4]),
+        Example([[0,1,2,3,4], -5, 99], [99, 0, 1, 2,3,4]),
+        Example([[0,1,2,3,4], -10, 99], [99, 0, 1, 2,3,4]),
         ]
 
     @classmethod
@@ -138,20 +138,20 @@ class insert_before ( Builtin_Function ):
         return old_list[0:position] + [new_item] + old_list[position:]
 
 class remove ( Builtin_Function ):
-    """Return a new list which is the old-list except that the item at the given position has been removed."""
+    """Return a new list which is old-list except that the item at the given ?position has been removed."""
 
     args =  ['old-list', 'position']
     result = 'new-list'
 
     examples = [
-        Example([[0,1,2,3,4], 0], [2,3,4]),
-        Example([[0,1,2,3,4], 1], [0,1,3,4]),
-        Example([[0,1,2,3,4], 4], [0,1,2,3,4]),
+        Example([[0,1,2,3,4], 0], [1,2,3,4]),
+        Example([[0,1,2,3,4], 1], [0,2,3,4]),
+        Example([[0,1,2,3,4], 4], [0,1,2,3]),
         Example([[0,1,2,3,4], 5], [0,1,2,3,4]),
         Example([[0,1,2,3,4], 6], [0,1,2,3,4]),
-        Example([[0,1,2,3,4], -1], [0,1,2,3,4]),
-        Example([[0,1,2,3,4], -5], [2,3,4]),
-        Example([[0,1,2,3,4], -6], [2,3,4]),
+        Example([[0,1,2,3,4], -1], [0,1,2,3]),
+        Example([[0,1,2,3,4], -5], [1,2,3,4]),
+        Example([[0,1,2,3,4], -6], [1,2,3,4]),
         ]
 
     @classmethod
@@ -170,8 +170,8 @@ class count( Builtin_Function ):
 
     examples = [
         Example([[0,1,2,3,4]], 5),
-        Example([[1]], 1),
-        Example([[0,1,1,1]], 3),
+        Example([[0]], 1),
+        Example([[0,0,0]], 3),
         Example([[]], 0),
         ]
 
@@ -186,8 +186,24 @@ class get ( Builtin_Function ):
     args =  ['list', 'position']
     result = 'item'
 
+    from_xpath = False
+
+    examples = [
+        Example([[0,1,2,3,4], 0], 0),
+        Example([[0,1,2,3,4], 1], 1),
+        Example([[0,1,2,3,4], 4], 4),
+        Example([[0,1,2,3,4], -1], 4),
+        Example([[0,1,2,3,4], -5], 0),
+        Example([[0,1,2,3,4], -10], 0),
+        Example([[0,1,2,3,4], 5], "<em>unspecified</em>"),
+        ]
+
     @classmethod
     def run(self, list, position):
+        if position < 0:
+            position = len(list) + position
+            if position  < 0:
+                position = 0
         return list[position]
 
 class reverse ( Builtin_Function ):
@@ -197,7 +213,7 @@ class reverse ( Builtin_Function ):
     result = 'new-list'
     
     examples = [
-        Example([[0,1,2,3,4]], [5,4,3,2,1]),
+        Example([[0,1,2,3,4]], [4,3,2,1,0]),
         Example([[1]], [1]),
         Example([[]], []),
     ]
@@ -211,20 +227,22 @@ class reverse ( Builtin_Function ):
 class sublist ( Builtin_Function ):
     """Return a new list, containing (in order) the items starting at position 'start' and continuing up to, but not including, the 'stop' position.   The 'stop' position may be omitted, in which case it defaults to the length of the list."""
 
+    note = "sublist differs from xpath's subsequence function in using a 'stop' position parameter instead of a 'length' parameter (in addition to the name change, and the zero-based indexing."
+
     args =  ['list', 'start', 'stop']
     optional = ['stop']
     result = 'new-list'
 
     examples = [
         Example([[0,1,2,3,4], 0, 0], []),
-        Example([[0,1,2,3,4], 0,1], [1]),
-        Example([[0,1,2,3,4], 0, 4], [0,1,2,3,4]),
-        Example([[0,1,2,3,4], 0, -2], [0,1,2,3]),
-        Example([[0,1,2,3,4], 2, 4], [3,4]),
-        Example([[0,1,2,3,4], 2, -2], [3]),
+        Example([[0,1,2,3,4], 0,1], [0]),
+        Example([[0,1,2,3,4], 0, 4], [0,1,2,3]),
+        Example([[0,1,2,3,4], 0, -2], [0,1,2]),
+        Example([[0,1,2,3,4], 2, 4], [2,3]),
+        Example([[0,1,2,3,4], 2, -2], [2]),
         Example([[0,1,2,3,4], 0], [0,1,2,3,4]),
-        Example([[0,1,2,3,4], 3], [4]),
-        Example([[0,1,2,3,4], -2], [4]),
+        Example([[0,1,2,3,4], 3], [3,4]),
+        Example([[0,1,2,3,4], -2], [3,4]),
     ]
 
     @classmethod
@@ -235,20 +253,6 @@ class sublist ( Builtin_Function ):
             return list[start:stop]
 
 
-ineq_note = """
-
-This function compares list items to see if they are equal.  If they
-are the same item, this is clearly true.  If they are Literals, then
-... use lit-eq?  It might be false.  If they are rif:const or rif:iri,
-then we just don't know, so conceptually the function isn't evaluated
-until/unless it's equated to a data-value.
-
-+ collation?
-
-+ equality-tester?
-
-"""
-
 
 class index_of ( Builtin_Function ):
     """result is a list of integers i where compare_literal(get(list_1,i), match_value, collation)"""
@@ -256,11 +260,11 @@ class index_of ( Builtin_Function ):
     args =  ['list', 'match-value', 'collation']
     result = 'listOfIndices'
 
-    ineq_note = True
+    uses_equality = True
 
     examples = [
-        Example([[0,1,2,3,4], 2], [1]),
-        Example([[0,1,2,3,4,5,2,2], 2], [0,1,5,6]),
+        Example([[0,1,2,3,4], 2], [2]),
+        Example([[0,1,2,3,4,5,2,2], 2], [2,6,7]),
         Example([[2,2,3,4,5,2,2], 2], [0,1,5,6]),
         Example([[2,2,3,4,5,2,2], 1], []),
         ]
@@ -273,8 +277,9 @@ class index_of ( Builtin_Function ):
                 result.append(position)
         return result
         
-class contains_item  ( Builtin_Predicate ):
-    
+class list_contains  ( Builtin_Predicate ):
+    """True only if ?item is datatype-equal some item in ?list (using the given ?collation, if provided."""
+
     args = ['list', 'item', 'collation']
     optional = ['collation']
 
@@ -295,18 +300,20 @@ class contains_item  ( Builtin_Predicate ):
                 return True
         return False
 
-    ineq_note = True
+    uses_equality = True
 
 
 class union ( Builtin_Function ):
+    """Returns a new list which contains exactly those items which are in list-1 or in list-2.   The order of the items in the new list is not specified."""
 
-    args =  ['list-1', 'list-2']
+    args =  ['list-1', 'list-2', 'collation']
+    optional = ['collation']
     result = 'new-list'
 
-    ineq_note = True
+    uses_equality = True
 
     examples = [
-        Example([[0,1,2,4], [3,4,5,6]], [0,1,2,4,5,3,6], "But output order isn't specified"),
+        Example([[0,1,2,4], [3,4,5,6]], [0,1,2,4,3,5,6], "But output order isn't specified"),
         ]
 
     @classmethod
@@ -321,61 +328,69 @@ class union ( Builtin_Function ):
         return distinct_values.run(list_1 + list_2, collation)
 
 class intersect ( Builtin_Function ):
+    """Returns a new list which contains exactly those items which are in list-1 and in list-2.   The order of the items in the new list is not specified."""
 
-    args =  ['list-1', 'list-2']
+    args =  ['list-1', 'list-2', 'collation']
+    optional = ['collation']
     result = 'new-list'
 
-    ineq_note = True
+    uses_equality = True
 
     @classmethod
     def run(self, list_1, list_2, collation=None):
         result = []
         for item in list_1:
-            if contains_item.run(list_2, item, collation):
+            if list_contains.run(list_2, item, collation):
                 result.append(item)
         return result
 
 class except__ ( Builtin_Function ):
+    """Returns a new list which contains exactly those items which are in list-1 and not in list-2.   The order of the items in the new list is not specified."""
 
-    args =  ['list-1', 'list-2']
+    args =  ['list-1', 'list-2', 'collation']
+    optional = ['collation']
     result = 'new-list'
 
-    ineq_note = True
+    uses_equality = True
 
     @classmethod
     def run(self, list_1, list_2, collation=None):
         result = []
         for item in list_1:
-            if not contains_item.run(list_2, item, collation):
+            if not list_contains.run(list_2, item, collation):
                 result.append(item)
         return result
 
 
 class distinct_values ( Builtin_Function ):
+    """Returns a new list which contains exactly those items which are in list-1, but without any two items being equal.   The order of the items in the new list is not specified."""
 
-    args =  ['old-list']
+    args =  ['old-list', 'collation']
     result = 'new-list'
 
-    ineq_note = True
+    uses_equality = True
 
     @classmethod
     def run(self, list_1, list_2, collation=None):
         result = []
         for item in list_1:
-            if not contains_item.run(result, item, collation):
+            if not list_contains.run(result, item, collation):
                 result.append(item)
         return result
 
 class delete( Builtin_Function ):
 
-    """Return a list (new-list) which contains all item in old-list,
-    in the same order, except those which are literal-equal to
-    match-value (in the given or default collation)."""
+    """Return a list which contains all item in old-list,
+    in the same order except those which are datatype-equal to
+    ?match-value."""
 
     args =  ['old-list', 'match-value', 'collation']
+    optional = ['collation']
     result = 'new-list'
 
-    ineq_note = True
+    from_xpath = True
+
+    uses_equality = True
 
     @classmethod
     def run(self, list, match_value, collation=None):
@@ -389,7 +404,7 @@ class delete( Builtin_Function ):
                 return True
         return False
 
-    ineq_note = True
+    uses_equality = True
 
 
 
@@ -398,7 +413,7 @@ class compare_literal : # DONT CALL IT A ( Builtin_Predicate ):
 
     args =  ['value_1', 'value_2', 'collation']
 
-    ineq_note = True
+    uses_equality = True
 
     examples = [
         Example([[0,1,2,3,4], []], 1),
@@ -428,7 +443,7 @@ class compare_literal : # DONT CALL IT A ( Builtin_Predicate ):
             raise RuntimeError('string compare with collation - not implemented')
         return cmp(value_1, value_2)
 
-    ineq_note = True
+    uses_equality = True
 
 
 # what if it's not a string....???
@@ -480,13 +495,100 @@ def p(i):
             print '  ** really', result
         
 
-print 'predicates:'
-for i in predicates:
-    p(i)
-print
+if False:
+    print 'predicates:'
+    for i in predicates:
+        q(i)
+    print
 
-print 'functions:'
-for i in functions:
-    p(i)
+    print 'functions:'
+    for i in functions:
+        p(i)
 
+def sub(s):
+    return s.replace('list-1', 'list<sub>1</sub>').replace('list-2', 'list<sub>2</sub>').replace('old-list', 'list<sub>1</sub>')
+
+def qm(s):
+    if s.startswith('?'):
+        return s
+    return '?'+s
+
+def h(i):
+
+    if i.from_xpath:
+        if i.from_xpath == True:
+            xname = i.name()
+        else:
+            xname = i.from_xpath
+        adapted = "(adapted from <tt>[http://www.w3.org/TR/xpath-functions/#func-%s fn:%s]</tt>) " % (xname, xname)
+    else:
+        adapted = ""
+
+    args = " ".join([qm(str(x)) for x in i.args])
+
+    print ""
+    print ""
+    print ""
+    print ""
+    print "==== <tt>func:%s</tt> %s====" % (i.name(), adapted)
+    print "<ul>"
+    print "<li>"
+    print "<em>Schema</em>:"
+    print sub("<p><tt>(%s; func:%s(%s))</tt></p>" % (args, i.name(), args))
+
+    if i.optional:
+        args2 = " ".join([qm(str(x)) for x in i.args if x not in i.optional])
+        print sub("<p><tt>(%s; func:%s(%s))</tt></p>" % (args2, i.name(), args2))
+
+    print "</li>"
+    print "<li>"
+    print "<em>Domains</em>:"
+    print "<p>... (to do)</p>"
+    print "</li>"
+    print "<li>"
+    print "<em>Informal Mapping</em>"
+    print "<p>"+sub(str(i.__doc__))+"</p>"
+
+
+    print "<p>If an argument value is outside of its domain, the value of the function is left unspecified.</p>"
+    print "</li>"
+    if getattr(i, 'examples', False):
+        print "<li>"
+        print "<em>Examples</em>"
+        print "<pre>"
+        for e in getattr(i, 'examples', []):
+            s = "%s(%s) = %s" % (i.name(), " ".join([str(x) for x in e.args]), e.result)
+            s = s.replace('[', 'list(').replace(']', ')').replace(",", " ")
+            print s
+
+            try:
+                result = i.run(*e.args)
+            except IndexError:
+                result = "<em>unspecified</em>"
+
+            if result != e.result:
+                print >>sys.stderr, s, '  ** really', result
+        print "</pre>"
+        print "</li>"
+    print "</ul>"
+
+
+if True:
+
+
+    print ""
+    print ""
+    print "=== Functions on RIF Lists ==="
+    print ""
+    print ""
+    for i in functions:
+        h(i)
+
+    print ""
+    print "=== Predicates on RIF Lists ==="
+    print ""
+    print ""
+    for i in predicates:
+        h(i)
+    print
 
